05671ecf83c0cae2ee87c0c4402988c4292132b3
hh2501 <hirotoito2501@gmail.com>
2026-01-30 18:57:53 +0900

    Move search tips link to footer and remove arrow icon

--- app/page.tsx
+++ app/page.tsx
@@ -60,31 +60,12 @@ export default function Home() {
           <HomeContent />
         </Suspense>
 
-        {/* 検索のコツへのリンク */}
-        <div className="mt-6 text-center">
-          <Link
-            href="/tips"
-            className="md-text-button inline-flex items-center gap-2"
-          >
-            検索のコツを見る
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-4 w-4"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <path d="m9 18 6-6-6-6" />
-            </svg>
-          </Link>
-        </div>
-
         {/* ページ最下部: モバイルでは content-visibility で描画遅延 */}
         <div className="mt-12 pt-8 border-t border-gray-200 content-below-fold">
           <div className="flex gap-4 justify-center items-center flex-wrap">
+            <Link href="/tips" className="md-text-button">
+              検索のコツを見る
+            </Link>
             <a href="/about" className="md-text-button">
               このサイトについて
             </a>
