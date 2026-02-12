b1e464d6dcd98a32ccd2c61ea6e84afbeeb42266
hh2501 <hirotoito2501@gmail.com>
2026-02-13 04:45:29 +0900

    style: contain toggle thumb within track with margins

--- app/components/HomeContent.tsx
+++ app/components/HomeContent.tsx
@@ -449,7 +449,7 @@ export default function HomeContent() {
               }`}
             />
             <span
-              className={`pointer-events-none absolute left-0.5 top-1/2 inline-block h-5 w-5 -translate-y-1/2 rounded-full bg-white shadow-sm ring-0 transition-transform ${
+              className={`pointer-events-none absolute left-1 top-1/2 inline-block h-4 w-4 -translate-y-1/2 rounded-full bg-white shadow-sm ring-0 transition-transform ${
                 exactMatchMode ? "translate-x-5" : "translate-x-0"
               }`}
             />
